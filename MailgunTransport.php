5876769b03a43dbc88a7174aa9e8b5fbdf89fd5b
Damien Villeneuve <d.villeneuve@beapp.fr>
2018-11-20 19:09:49 +0100

    Add monorepo-builder dependency to help maintaining multiple repos

---
+++ MailgunTransport.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Beapp\Email\Transport\Mailgun;
+
+use Beapp\Email\Core\Mail;
+use Beapp\Email\Core\MailerException;
+use Beapp\Email\Core\Transport\MailerTransport;
+use Mailgun\Mailgun;
+
+class MailgunTransport implements MailerTransport
+{
+
+    /** @var string */
+    private $key;
+    /** @var string */
+    private $domain;
+    /** @var Mailgun $mailgun */
+    private $client;
+
+    /**
+     * @param string $key
+     * @param string $domain
+     */
+    public function __construct(string $key, string $domain)
+    {
+        $this->key = $key;
+        $this->domain = $domain;
+    }
+
+    protected function getClient(): Mailgun
+    {
+        if (is_null($this->client)) {
+            $this->client = Mailgun::create($this->key);
+        }
+        return $this->client;
+    }
+
+    /**
+     * Delivers the email to the recipients through a specific channel (direct call to client, publish to AMQP server, etc...)
+     *
+     * @param Mail $email
+     * @throws MailerException
+     */
+    public function sendEmail(Mail $email): void
+    {
+        $mailParams = [];
+        $mailParams['from'] = $email->getSenderEmail();
+
+        if ($email->getRecipientName()) {
+            $mailParams['to'] = $email->getRecipientName() . ' <' . $email->getRecipientEmail() . '>';
+        } else {
+            $mailParams['to'] = $email->getRecipientEmail();
+        }
+        $mailParams['subject'] = $email->getSubject();
+        $mailParams['text'] = $email->getTextContent();
+        $mailParams['html'] = $email->getHtmlContent();
+
+        try {
+            $this->getClient()->messages()->send($this->domain, $mailParams);
+        } catch (\Exception $e) {
+            throw new MailerException("Couldn't send mail through Mailgun", 0, $e);
+        }
+    }
+}
